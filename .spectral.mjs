0e9b624b5fb7edbf30146ebec2d687144928d888
Jakub Rożek <jakub@rozek.tech>
2023-03-06 17:11:20 +0100

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15524.stoplight-dev.com/api/v1/projects/cHJqOjIxMQ/spectral.js?branch_slug=master&token=06e9f472-35b6-4175-aa72-65556d5a93be";
+export default { extends: ruleset };
